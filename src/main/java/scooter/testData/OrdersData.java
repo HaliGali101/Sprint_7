e2addee0108c8826a02bd2e89b73fce90be6ef28
Alexandr Petrov <HaliGali101@yandex.ru>
2025-06-25 12:15:44 +0300

    develop/ исправлена генерация объекта заказа при помощи javafaker

--- src/main/java/scooter/testData/OrdersData.java
+++ src/main/java/scooter/testData/OrdersData.java
@@ -1,22 +1,35 @@
 package scooter.testData;
 
+import com.github.javafaker.Faker;
 import scooter.pojo.Orders;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class OrdersData {
 
     public static Orders getOrderData(ArrayList<String> colorsArray) {
-        String firstName = "Тестовый";
-        String lastName = "Тест";
-        String address = "Москва, пр.Мира д.120";
-        int metroStation = 9;
-        String phone = "+7-911-111-11-11";
-        int rentTime = 3;
-        String deliveryDate = "2025-04-05";
-        String comment = "Не звонить после 6!";
+        Faker faker = new Faker();
+
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String address = faker.address().fullAddress();
+        int metroStation = faker.number().numberBetween(0, 20);
+        String phone = faker.phoneNumber().phoneNumber();
+        int rentTime = faker.number().numberBetween(0, 24);
+        String deliveryDate = getDate(faker);
+        String comment = faker.lorem().sentence();
 
         return new Orders(firstName, lastName, address, metroStation, phone, rentTime, deliveryDate, comment, colorsArray);
+    }
+
+    private static String getDate(Faker faker) {
+        Date futureDate = faker.date().future(365, TimeUnit.DAYS);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(futureDate);
     }
 
 }
