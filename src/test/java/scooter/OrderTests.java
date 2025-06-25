f08ba99301745c37bb0ca7d1592f83065318d02b
Alexandr Petrov <HaliGali101@yandex.ru>
2025-06-25 11:52:39 +0300

    develop/ добавлена параметризация в тестовый класс OrderTests.java, согласно условию задачи

--- src/test/java/scooter/OrderTests.java
+++ src/test/java/scooter/OrderTests.java
@@ -6,55 +6,47 @@ import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.ValidatableResponse;
 import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import scooter.pojo.Orders;
-import scooter.testData.ColorArrayData;
 import scooter.testData.OrdersData;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 
+@RunWith(Parameterized.class)
 public class OrderTests extends SetUp {
 
     private String track;
+
+    private final ArrayList<String> colorsArray;
+    private final int statusCode;
+
+    public OrderTests(ArrayList<String> colorsArray, int statusCode) {
+        this.colorsArray = colorsArray;
+        this.statusCode = statusCode;
+    }
+
+
+    @Parameterized.Parameters
+    public static Object[][] setUp() {
+        return new Object[][] {
+                {new ArrayList<>(List.of("BLACK")), 201},
+                {new ArrayList<>(List.of("GRAY")), 201},
+                {new ArrayList<>(Arrays.asList("BLACK", "GRAY")), 201},
+                {new ArrayList<>(List.of()),201}
+        };
+    }
 
     @Test
     @DisplayName("Создание заказа.Чёрный цвет")
     @Description("Создать заказ с чёрным цветом самоката")
     public void createOrderWithBlackColor() {
-        ValidatableResponse response = createOrder("black")
-                .statusCode(201)
-                .assertThat().body("track", notNullValue());
-
-        track = response.extract().path("track").toString();
-    }
-
-    @Test
-    @DisplayName("Создание заказа.Серый цвет")
-    @Description("Создать заказ с серым цветом самоката")
-    public void createOrderWithGrayColor() {
-        ValidatableResponse response = createOrder("gray")
-                .statusCode(201)
-                .assertThat().body("track", notNullValue());
-
-        track = response.extract().path("track").toString();
-    }
-
-    @Test
-    @DisplayName("Создание заказа.Любой цвет")
-    @Description("Создать заказ с чёрным или серым цветом самоката")
-    public void createOrderWithAllColors() {
-        ValidatableResponse response = createOrder("all")
-                .statusCode(201)
-                .assertThat().body("track", notNullValue());
-
-        track = response.extract().path("track").toString();
-    }
-
-    @Test
-    @DisplayName("Создание заказа.Без цвета")
-    @Description("Создать заказ без передачи цвета самоката")
-    public void createOrderWithoutColors() {
-        ValidatableResponse response = createOrder("null")
-                .statusCode(201)
+        ValidatableResponse response = createOrder(colorsArray)
+                .statusCode(statusCode)
                 .assertThat().body("track", notNullValue());
 
         track = response.extract().path("track").toString();
@@ -67,8 +59,8 @@ public class OrderTests extends SetUp {
     }
 
     @Step("Создать заказ")
-    private ValidatableResponse createOrder(String colorConfig) {
-        Orders order = OrdersData.getOrderData(new ColorArrayData().getColorData(colorConfig));
+    private ValidatableResponse createOrder(ArrayList<String> colorConfig) {
+        Orders order = OrdersData.getOrderData(colorConfig);
         return OrdersMethods.postCreateOrders(order);
     }
 
